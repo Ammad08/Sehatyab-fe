0f9e7d4899717945efdcfe348609e98bd34bf057
Ammad Arshad <ammadarshad08@gmail.com>
2025-03-06 21:36:42 +0500

    issuessolved

--- src/app/bookAppointment/page.tsx
+++ src/app/bookAppointment/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import AppointmentForm from '../component/BookAppointment/AppointmentForm'
-import ServicesHero from '../component/Services/ServicesHero'
+// import ServicesHero from '../component/Services/ServicesHero'
 import BookAppointmentHero from '../component/BookAppointment/BookAppointmentHero'
+import { Suspense } from "react";
 
 const page = () => {
   return (
-    <div>
+        <Suspense fallback={<div>Loading...</div>}>
+    
         {/* <ServicesHero/> */}
         <BookAppointmentHero/>
       <AppointmentForm/>
-    </div>
+          </Suspense>
+      
   )
 }
 
